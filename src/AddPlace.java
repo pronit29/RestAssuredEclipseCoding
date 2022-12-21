480c58df654a7113d855fb1ccf1e4497d7ad514c
pronit29-admin <pronit29@gmail.com>
2022-12-21 22:14:16 +0530

    First commit from eclipse

---
+++ src/AddPlace.java
@@ -0,0 +1,39 @@
+import io.restassured.RestAssured;
+import static io.restassured.RestAssured.given;
+
+//Validate if Add Place API is working fine as expected
+public class AddPlace {
+    public static void main(String args[]) {
+        final String jsonBody = "{\r\n"
+                + "  \"location\": {\r\n"
+                + "    \"lat\": -38.383494,\r\n"
+                + "    \"lng\": 33.427362\r\n"
+                + "  },\r\n"
+                + "  \"accuracy\": 50,\r\n"
+                + "  \"name\": \"Frontline house\",\r\n"
+                + "  \"phone_number\": \"(+91) 983 893 3937\",\r\n"
+                + "  \"address\": \"29, side layout, cohen 09\",\r\n"
+                + "  \"types\": [\r\n"
+                + "    \"shoe park\",\r\n"
+                + "    \"shop\"\r\n"
+                + "  ],\r\n"
+                + "  \"website\": \"http://google.com\",\r\n"
+                + "  \"language\": \"French-IN\"\r\n"
+                + "}";
+        String resourceURI = "/maps/api/place/add/json";
+
+        //Pre-requisite - Set up the baseURI
+        RestAssured.baseURI = "https://rahulshettyacademy.com";
+
+        //Given
+        given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+                .body(jsonBody)
+
+                //When
+                .when().post(resourceURI)
+
+                //Then
+                .then().log().all().assertThat().statusCode(200);
+
+    }
+}
